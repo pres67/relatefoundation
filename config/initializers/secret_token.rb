b0bfba60081581b47c961f0ff5f33581474d1758
Adam Larson <adam@relate-institute.org>
2011-09-23 13:34:24 -0600

    More updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Relatefoundation::Application.config.secret_token = '4163a6f5408312108e766d913b5b5972630a0d688fbe04aabceb19c442511dd12ff08cac5a06c279b8af89e72aca7f767681a31d8b05ce1657121a0cd967e5fd'
