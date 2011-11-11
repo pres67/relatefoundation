59dd34746a06e0056507f6244851f2ac7c8c4525
Adam Larson <adam@relate-institute.org>
2011-11-11 10:07:38 -0700

    bg image

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails3TrCheckout::Application.routes.draw do  
-  
-  resources :customer
-  
+    
   match 'payments/new' => 'payments#new', :as => :new_payment
   match 'payments/confirm' => 'payments#confirm', :as => :confirm_payment
   match 'payments/cancel' => 'payments#cancel', :as => :cancel_payment
